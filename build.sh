1d7e7306761f3bbab75dc0e606d34ef1406a0872
Tesfa Yohannes <tmyohannes@gmail.com>
2018-03-25 14:25:15 +0300

    Fix configuration copy location

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # setup config files
-cp config/config.js mediasoup-demo/app/
+cp config/config.js mediasoup-demo/server/
 # build dockers
 docker build . -t node:app
 docker build nginx/ -t nginx:latest
