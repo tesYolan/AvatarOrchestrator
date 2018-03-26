56f31831281f1d5134228e2655b5f1d35da9a822
Tesfa Yohannes <tmyohannes@gmail.com>
2018-03-26 05:05:18 +0300

    Add 3443 to docker-compose

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 # setup config files
 cp config/config.js mediasoup-demo/server/
+# TODO either the above or the below is wrong.
+cp config/config.js mediasoup-demo/app/
 # build dockers
 docker build . -t node:app
 docker build -t nginx:latest -f Dockerfile.nginx 
